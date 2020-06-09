c57700a7076fbb797d29a0d149b29eb9330920ce
Ed <b9012949@newcastle.ac.uk>
2020-06-09 18:46:17 +0100

    Added registration page with sign up form. Dosnt submit data yet

--- amazon_scraper/amazon_scraper/settings.py
+++ amazon_scraper/amazon_scraper/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'item_searcher.apps.ItemSearcherConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- amazon_scraper/amazon_scraper/urls.py
+++ amazon_scraper/amazon_scraper/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
     path('', include('item_searcher.urls')) #So the item_searcher app is the homepage of the entire website because of the empty ''
 ]
