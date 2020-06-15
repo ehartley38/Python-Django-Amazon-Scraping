6051076609161923e53a6f1b1a70a8e6819ef91d
Ed <b9012949@newcastle.ac.uk>
2020-06-15 19:15:16 +0100

    Completed part 9 - Allows user to update profile info. Also resized profile pics

--- amazon_scraper/users/models.py
+++ amazon_scraper/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE) #One to one relationship with user model
@@ -10,4 +11,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save() #Run the original save method of the parent class
+
+        img = Image.open(self.image.path) #Open image of the current instance
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size) #resize image
+            img.save(self.image.path)
 
